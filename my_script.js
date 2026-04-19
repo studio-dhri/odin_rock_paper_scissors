801c195b5db634ed804d450717f69b6db48096a0
studio-dhri <252181032+studio-dhri@users.noreply.github.com>
2026-04-19 15:56:42 +0200

    JS - Computer can now make a choice

--- my_script.js
+++ my_script.js
@@ -1,1 +1,26 @@
-console.log("Hello World from linked file");
+console.log("We are playing Rock Paper Scissors");
+
+function getCompputerChoice() {
+    let computerChoice = "randomText";
+    let controlNumber = 0;
+
+    controlNumber = Math.floor(Math.random() * 10) % 3;
+
+    switch (controlNumber) {
+        case 0:
+            computerChoice = "Rock";
+            break;
+        case 1:
+            computerChoice = "Paper";
+            break;
+        case 2:
+            computerChoice = "Scissors";
+            break;
+        default:
+            console.log("Computer failed to make a choice");
+    }
+
+    console.log(computerChoice);
+}
+
+getCompputerChoice();
